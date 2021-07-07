3764dc7cfcf1e92adfa0cca98cb0e17b600aeaf7
Pavlo <puhapavlo13@gmail.com>
2021-07-07 09:05:39 +0300

    modal

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#callback-button').click(function () {
+      event.preventDefault()
+      $('.modal').addClass('modal_active');
+    });
+  });
+
+$('.modal').mouseup(function (e) {
+    let modalContent = $(".modal__content");
+    if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+      $(this).removeClass('modal_active');
+    }
+  });
